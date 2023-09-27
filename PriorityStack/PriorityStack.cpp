318a57b534109b74f85fb34c59ff5cbfc609ed33
nnton3 <akarnomazov@gmail.com>
2023-09-27 10:26:45 +0300

    setup push

--- PriorityStack/PriorityStack.cpp
+++ PriorityStack/PriorityStack.cpp
@@ -5,22 +5,65 @@
 #include "stack.h"
 
 using std::cout;
+using std::endl;
 using std::string;
 using std::map;
 using std::stack;
 
 //namespace using std;
 
+template <typename T>
+class PriorityStackClass
+{
+public:
+    void Push(T value, int priority)
+    {
+        if (stacks_.find(priority) != stacks_.end())
+        {
+            stack<T> *targetStack = &stacks_[priority];
+            (*targetStack).push(value);
+        }
+        else
+        {
+            stack<T> stack;
+            stack.push(value);
+            stacks_[priority] = stack;
+        }
+    }
+    //void Push(T value);
+    T Pop()
+    {
+        if (stacks_.empty()) return NULL;
+        stack<T> stack = stacks_.begin()->second;
+        if (stack.empty()) return NULL;
+        return stack.top();
+    }
+    int Count()
+    {
+        int count = 0;
+        for (auto it = stacks_.begin(); it != stacks_.end(); ++it)
+            count++;
+        return count;
+    }
+private:
+    map<int, stack<T>> stacks_;
+};
+
 int main()
 {
     cout << "Hello World!\n";
     PriorityStackClass<string> stack;
-    /*stack.Push("Tom", 1);
-    stack.Push("Steve", 5);
-    stack.Push("Ana", 1);
-    stack.Push("Alex", 5);
-
-    auto count = stack.Count();
+    string tom = "Tom";
+    string steve = "Steve";
+    string ana = "Ana";
+    string alex = "Alex";
+    stack.Push(tom, 1);
+    stack.Push(steve, 5);
+    stack.Push(ana, 1);
+    stack.Push(alex, 5);
+    cout << stack.Pop() << endl;
+    stack.Count();
+    /*auto count = stack.Count();
     for (size_t i = 0; i < count; i++)
     {
         std::cout << stack.Pop() << "\n";
--- PriorityStack/stack.h
+++ PriorityStack/stack.h
@@ -3,14 +3,14 @@
 #include <stack>
 #include <map>
 
-template <typename T>
-class PriorityStackClass
-{
-public:
-    void Push(T value, int priority);
-    void Push(T value);
-    T Pop();
-    int Count();
-private:
-    std::map<int, std::stack<T>> stacks_;
-};
+//template <typename T>
+//class PriorityStackClass
+//{
+//public:
+//    template<typename T> void Push(T value, int priority);
+//    //void Push(T value);
+//    template<typename T> T Pop();
+//    int Count();
+//private:
+//    std::map<int, std::stack<T>> stacks_;
+//};
