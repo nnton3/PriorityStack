72c6ae4813a27f6991e14a2e2cbd76d68945bb11
nnton3 <akarnomazov@gmail.com>
2023-09-28 22:09:58 +0300

    complete

--- PriorityStack/PriorityStack.cpp
+++ PriorityStack/PriorityStack.cpp
@@ -7,65 +7,25 @@
 using std::cout;
 using std::endl;
 using std::string;
-using std::map;
-using std::stack;
-
-//namespace using std;
-
-template <typename T>
-class PriorityStackClass
-{
-public:
-    void Push(T value, int priority)
-    {
-        if (stacks_.find(priority) != stacks_.end())
-        {
-            stack<T> *targetStack = &stacks_[priority];
-            (*targetStack).push(value);
-        }
-        else
-        {
-            stack<T> stack;
-            stack.push(value);
-            stacks_[priority] = stack;
-        }
-    }
-    //void Push(T value);
-    T Pop()
-    {
-        if (stacks_.empty()) return NULL;
-        stack<T> stack = stacks_.begin()->second;
-        if (stack.empty()) return NULL;
-        return stack.top();
-    }
-    int Count()
-    {
-        int count = 0;
-        for (auto it = stacks_.begin(); it != stacks_.end(); ++it)
-            count++;
-        return count;
-    }
-private:
-    map<int, stack<T>> stacks_;
-};
 
 int main()
 {
-    cout << "Hello World!\n";
     PriorityStackClass<string> stack;
+    
     string tom = "Tom";
     string steve = "Steve";
     string ana = "Ana";
     string alex = "Alex";
+    
     stack.Push(tom, 1);
     stack.Push(steve, 5);
     stack.Push(ana, 1);
     stack.Push(alex, 5);
-    cout << stack.Pop() << endl;
-    stack.Count();
-    /*auto count = stack.Count();
-    for (size_t i = 0; i < count; i++)
+    stack.Push(alex, 2);
+
+    int count = stack.Count();
+    for (int i = 0; i < count; i++)
     {
-        std::cout << stack.Pop() << "\n";
-    }*/
+        cout << stack.Pop() << endl;
+    }
 }
--- PriorityStack/stack.h
+++ PriorityStack/stack.h
@@ -3,14 +3,59 @@
 #include <stack>
 #include <map>
 
-//template <typename T>
-//class PriorityStackClass
-//{
-//public:
-//    template<typename T> void Push(T value, int priority);
-//    //void Push(T value);
-//    template<typename T> T Pop();
-//    int Count();
-//private:
-//    std::map<int, std::stack<T>> stacks_;
-//};
+using std::map;
+using std::stack;
+
+template <typename T>
+class PriorityStackClass
+{
+public:
+    void Push(T value, int priority)
+    {
+        if (stacks_.find(priority) != stacks_.end())
+        {
+            stack<T>* targetStack = &stacks_[priority];
+            (*targetStack).push(value);
+        }
+        else
+        {
+            stack<T> stack;             //записываю копию или тот который сдесь создал?
+            stack.push(value);
+            stacks_[priority] = stack;
+        }
+    }
+
+    void Push(T value)
+    {
+        Push(value, 0);
+    }
+
+    T Pop()
+    {
+        if (stacks_.empty()) return NULL;
+        for (auto it = stacks_.begin(); it != stacks_.end(); ++it)
+        {
+            stack<T>& stack = it->second;
+            if (stack.empty())
+                continue;
+            T res = stack.top();    //Если брать по ссылке, то после pop данные удаляются из памяти?!
+            stack.pop();
+            return res;
+        }
+        return NULL;                //TODO: drop ex
+    }
+
+    int Count()
+    {
+        int count = 0;
+        for (auto it = stacks_.begin(); it != stacks_.end(); ++it)
+        {
+            count += (it->second).size();
+        }
+
+        return count;
+    }
+
+private:
+    map<int, stack<T>> stacks_;
+};
